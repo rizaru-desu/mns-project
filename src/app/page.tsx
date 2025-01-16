b70d7eb9348eeaf303beb50b9c53a27d717cb89d
rizarudesu <rizal.achmad@visionet.co.id>
2025-01-16 23:44:04 +0700

    login & register UI
    - tab menu
    - login
    - register
    - phone input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,220 @@
-import Image from "next/image";
+"use client";
+import {
+  Layout,
+  Tabs,
+  Form,
+  Input,
+  Checkbox,
+  Button,
+  Flex,
+  Image,
+  Typography,
+  Space,
+} from "antd";
+import { FaUserAlt } from "react-icons/fa";
+import { FaLock } from "react-icons/fa6";
 
-export default function Home() {
+import PhoneInput from "react-phone-input-2";
+import "react-phone-input-2/lib/style.css";
+
+type FieldLoginType = {
+  username?: string;
+  password?: string;
+  remember?: string;
+};
+
+type FieldRegisterType = {
+  email?: string;
+  password?: string;
+  confirmPassword?: string;
+  phone?: string;
+  code?: number;
+};
+
+const { Content, Footer } = Layout;
+const { Title, Text } = Typography;
+
+export default function Page() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <Layout className="min-h-screen flex flex-col">
+      <Content className="flex-1 flex flex-col items-center justify-center bg-[url('/image/bg.png')] bg-contain bg-center bg-no-repeat">
+        <Flex align="center" className="my-10">
+          <Image
+            preview={false}
+            src="/image/molecule.png"
+            alt="logo"
+            className="!w-12 !h-12 mx-5"
+          />
+          <Flex vertical>
+            <Title>NMS Monitoring</Title>
+            <Text type="secondary">ZABBIX & PRTG Monitoring Tools</Text>
+          </Flex>
+        </Flex>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <Tabs
+          className="w-1/3"
+          animated
+          size="large"
+          type="card"
+          items={[
+            {
+              label: "Login",
+              key: "login",
+              children: (
+                <Form
+                  name="login-form"
+                  style={{ maxWidth: 600 }}
+                  initialValues={{ remember: true }}
+                  autoComplete="off"
+                >
+                  <Form.Item<FieldLoginType>
+                    name="username"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please input your username!",
+                      },
+                    ]}
+                  >
+                    <Input prefix={<FaUserAlt />} placeholder="Username" />
+                  </Form.Item>
+
+                  <Form.Item<FieldLoginType>
+                    name="password"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please input your password!",
+                      },
+                    ]}
+                  >
+                    <Input.Password
+                      prefix={<FaLock />}
+                      placeholder="Password"
+                    />
+                  </Form.Item>
+
+                  <Flex justify="space-between" align="center">
+                    <Form.Item<FieldLoginType>
+                      name="remember"
+                      valuePropName="checked"
+                      label={null}
+                    >
+                      <Checkbox>Remember me</Checkbox>
+                    </Form.Item>
+                    <Button type="link">Forgot your password</Button>
+                  </Flex>
+
+                  <Form.Item label={null}>
+                    <Button type="primary" htmlType="submit">
+                      Sign In
+                    </Button>
+                  </Form.Item>
+                </Form>
+              ),
+            },
+            {
+              label: "Sign Up",
+              key: "register",
+              children: (
+                <Form
+                  name="register-form"
+                  style={{ maxWidth: 600 }}
+                  initialValues={{ remember: true }}
+                  autoComplete="off"
+                >
+                  <Form.Item<FieldRegisterType>
+                    name="email"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please input your email!",
+                      },
+                      {
+                        type: "email",
+                        message: "The input is not valid E-mail!",
+                      },
+                    ]}
+                  >
+                    <Input prefix={<FaUserAlt />} placeholder="Email" />
+                  </Form.Item>
+
+                  <Form.Item<FieldRegisterType>
+                    name="password"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please input your password!",
+                      },
+                    ]}
+                  >
+                    <Input.Password
+                      prefix={<FaLock />}
+                      placeholder="Password"
+                    />
+                  </Form.Item>
+
+                  <Form.Item<FieldRegisterType>
+                    name="password"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please input your confirm password!",
+                      },
+                    ]}
+                  >
+                    <Input.Password
+                      prefix={<FaLock />}
+                      placeholder="Confirm Password"
+                    />
+                  </Form.Item>
+
+                  <Form.Item<FieldRegisterType>
+                    name="phone"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please input your phone number!",
+                      },
+                    ]}
+                  >
+                    <PhoneInput
+                      country={"id"}
+                      inputStyle={{ width: "100%" }}
+                      containerStyle={{ width: "100%" }}
+                    />
+                  </Form.Item>
+
+                  <Form.Item<FieldRegisterType>
+                    name="code"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please input your verification code!",
+                      },
+                    ]}
+                  >
+                    <Space direction="horizontal">
+                      <Input placeholder="input Verification Code" />
+                      <Button onClick={() => {}}>Get Code</Button>
+                    </Space>
+                  </Form.Item>
+
+                  <Form.Item label={null}>
+                    <Button type="primary" htmlType="submit">
+                      Sign Up
+                    </Button>
+                  </Form.Item>
+                </Form>
+              ),
+            },
+          ]}
+        />
+      </Content>
+
+      <Footer className="text-center py-4 bg-gray-100">
+        © {new Date().getFullYear()} Produced by PT Hipernet Indodata.
+      </Footer>
+    </Layout>
   );
 }
